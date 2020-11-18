89a0d6781a51fc8e7ad2f93fc54145d75df9ef6a
renderVis <rendervis333@gmail.com>
2020-11-18 16:21:22 +0200

    storage

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,60 @@
 import { makeSprite, GameProps } from '@replay/core';
 
 import { Level } from './level';
+import { Menu } from './menu';
 
-export const Game = makeSprite<GameProps>({
-  render() {
+type GameState = {
+  view: 'menu' | 'level';
+  attempt: number;
+  highScore: number;
+};
+
+export const Game = makeSprite<GameProps, GameState>({
+  init({ device }) {
+    const store = device.storage.getStore();
+    return {
+      view: 'menu',
+      attempt: 0,
+      highScore: Number(store.highScore || '0'),
+    };
+  },
+
+  render({ state, updateState, device }) {
+    const inMenuScreen = state.view === 'menu';
     return [
       Level({
-        id: 'level',
+        id: `level-${state.attempt}`,
+        paused: inMenuScreen,
+        gameOver: (score) => {
+          updateState((prevState) => {
+            let { highScore } = prevState;
+            if (score > highScore) {
+              highScore = score;
+              device.storage.setStore({ highScore: String(highScore) });
+            }
+            return {
+              ...prevState,
+              view: 'menu',
+              highScore,
+            };
+          });
+        },
       }),
+      inMenuScreen
+        ? Menu({
+            id: 'menu',
+            highScore: state.highScore,
+            start: () => {
+              updateState((prevState) => {
+                return {
+                  ...prevState,
+                  view: 'level',
+                  attempt: prevState.attempt + 1,
+                };
+              });
+            },
+          })
+        : null,
     ];
   },
 });
---
+++ src/menu.ts
@@ -0,0 +1,31 @@
+import { makeSprite, t } from '@replay/core';
+import { WebInputs } from '@replay/web';
+import { iOSInputs } from '@replay/swift';
+
+type MenuProps = {
+  start: () => void;
+  highScore: number;
+};
+
+export const Menu = makeSprite<MenuProps, undefined, WebInputs | iOSInputs>({
+  render({ props, device }) {
+    const { inputs } = device;
+
+    if (inputs.pointer.justReleased || inputs.keysJustPressed[' ']) {
+      props.start();
+    }
+    return [
+      t.text({
+        text: device.isTouchScreen ? 'Tap to Start' : 'Click or Space Bar to Start',
+        color: 'white',
+        y: 100,
+      }),
+      t.text({
+        text: `High score: ${props.highScore}`,
+        font: { name: 'Courier', size: 24 },
+        color: 'white',
+        y: 150,
+      }),
+    ];
+  },
+});
